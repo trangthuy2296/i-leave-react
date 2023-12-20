5d677b3af8ad347be4e9bdaa00d28d0560c4e464
nellie1710 <nhulnk1710@gmail.com>
2023-12-20 09:12:19 +0700

    change name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} alt="logo" />
         <p>
-          iLeave
+          ileave
         </p>
         <a
           className="App-link"
