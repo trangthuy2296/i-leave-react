7a04a032f2120c74d42c0cfbd978cfcadc575b69
barrytran209 <barry.tran@manadr.com>
2023-12-21 14:27:08 +0700

    Remove bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
 import logo from './logo.svg';
 import Login from './Login';
 import Dashboard from './Dashboard';
