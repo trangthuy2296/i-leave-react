517173060abef95f1c741964c727902e5931cbe9
barrytran209 <barry.tran@manadr.com>
2023-12-21 17:58:37 +0700

    Update router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+//css
 import './App.css';
+//package
+import React from 'react';
+import {   Route, createBrowserRouter, createRoutesFromElements,defer } from "react-router-dom";
+//page
 import Login from './Login';
 import Dashboard from './Dashboard';
-import { ProtectedRoute } from './Component/ProtectedRoutes';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ProtectedLayout } from './Component/ProtectedLayout';
+import { AuthLayout } from './Component/AuthLayout';
 
-
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />}/>
-        <Route path="/Dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}  />
-      </Routes>
-    </Router>
+const getUserData = () =>
+  new Promise((resolve) =>
+    setTimeout(() => {
+      const user = window.localStorage.getItem("user");
+      resolve(user);
+    }, 3000)
   );
-}
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      element={<AuthLayout />}
+      loader={() => defer({ userPromise: getUserData() })}
+    >
+
+        <Route path="/" element={<ProtectedLayout><Dashboard /></ProtectedLayout>} />
+        <Route path="/Login" element={<Login />} />
+    </Route>
+  )
+);
+
+
 //test branch
