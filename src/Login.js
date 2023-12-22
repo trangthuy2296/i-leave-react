c476ddd3a26d523cf1065ff87e425a96b2cbc700
nellie1710 <nhulnk1710@gmail.com>
2023-12-22 15:40:55 +0700

    Update input form to be responsive

--- src/Login.js
+++ src/Login.js
@@ -2,16 +2,18 @@
 import React from 'react';
 import { Form, Input, Button } from 'antd';
 import bg from './Images/img-login.png';
-import icon from './Images/ileave-icon.png';
-import { Formik } from "formik";
+import logo from './Images/ileave-icon.png';
+import { Formik, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 // Creating schema
 const schema = Yup.object().shape({
-  email: Yup.string()
+  email: Yup
+    .string()
     .required("Email is required")
     .email("Invalid email format"),
-  password: Yup.string()
+  password: Yup
+    .string()
     .required("Password is required")
     .min(8, "Password must be at least 8 characters"),
 });
@@ -21,6 +23,8 @@ const loginBGRGB = 'rgb(236,213,255)';
 const Login = () => {
   return (
     <div className="login-container">
+
+      {/*Left section*/}
       <div className="image-container" style={{ backgroundColor: loginBGRGB }}>
         {/* Add your big picture here */}
         <img
@@ -29,76 +33,74 @@ const Login = () => {
           className="login-image"
         />
       </div>
+
+      {/*Right section*/}  
       <div className="form-container">
         <div className="login-header">
-        <img
-          src={icon}
-          alt="ileave icon"
-        />
+          <img
+            src={logo}
+            alt="ileave icon"
+          />
           <h1> Sign in to iLeave</h1>
           <p style={{ color: '#6A7882' }}>Welcome back!</p>
         </div>
-        <Formik
-        validationSchema={schema}
-        initialValues={{ email: '', password: '' }}
-        onSubmit={(values) => {
-          alert(JSON.stringify(values));
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-        }) => (
-<Form onFinish={handleSubmit} className="login-form">
-            <Form.Item
-              label={<span style={{ fontWeight: 'bold', fontSize: '12px' }}>Email</span>}
-              name="email"
-              validateStatus={errors.email && touched.email ? 'error' : ''}
-              help={errors.email && touched.email && errors.email}
-              labelCol={{ span: 24 }}
-              //wrapperCol={{ span: 24 }}
-              style={{ marginBottom: '16px' }}
-            >
-              <Input
-                placeholder="Enter your email"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-               
-              />
-            </Form.Item>
 
-            <Form.Item
-              label={<span style={{ fontWeight: 'bold', fontSize: '12px' }}>Password</span>}
-              name="password"
-              validateStatus={errors.password && touched.password ? 'error' : ''}
-              help={errors.password && touched.password && errors.password}
-              labelCol={{ span: 24 }}
-              //wrapperCol={{ span: 24 }}
-            >
-              <Input.Password
-                placeholder="Enter your password"
-                name="password"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-            </Form.Item>
-            
-            <Form.Item 
-              wrapperCol={{ span: 24 }}
+        {/*Sign in form*/}
+        <Formik
+          validationSchema={schema}
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values) => {
+            alert(JSON.stringify(values));
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+          }) => (
+            <Form onFinish={handleSubmit} className="login-form" layout="vertical">
+              <Form.Item
+                label={<label>Email</label>}
+                name="email"
+                validateStatus={errors.email && touched.email ? 'error' : ''}
+                help={errors.email && touched.email && <span className="error-message-form">{errors.email}</span>}
               >
-              <Button type="primary" htmlType="submit" className="primary-button">
-                Sign in {/* Use the "primary-button" class */}
-              </Button>
-            </Form.Item>
-          </Form>
-        )}
-      </Formik>
+                <Input
+                  placeholder="Enter your email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Form.Item>
+
+              <Form.Item
+                label={<label>Password</label>}
+                name="password"
+                validateStatus={errors.password && touched.password ? 'error' : ''}
+                help={errors.password && touched.password && <span className="error-message-form">{errors.password}</span>}
+              >
+                <Input.Password
+                  placeholder="Enter your password"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Form.Item>
+
+              <Form.Item
+                wrapperCol={{ span: 24 }}
+              >
+                <Button type="primary" htmlType="submit" className="primary-button">
+                  Sign in {/* Use the "primary-button" class */}
+                </Button>
+              </Form.Item>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
