0ce9b6beab5e08d8f487f053be379f907817298f
barrytran209 <barry.tran@manadr.com>
2023-12-22 13:36:14 +0700

    Fix delay

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ import { ProtectedLayout } from './Component/ProtectedLayout';
 import { AuthLayout } from './Component/AuthLayout';
 
 const getUserData = () =>
-  new Promise((resolve) =>
-    setTimeout(() => {
-      const user = window.localStorage.getItem("user");
-      resolve(user);
-    }, 3000)
-  );
-
+{}
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route
@@ -24,7 +18,7 @@ export const router = createBrowserRouter(
       loader={() => defer({ userPromise: getUserData() })}
     >
         <Route path="/" element={<ProtectedLayout><Dashboard /></ProtectedLayout>} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
     </Route>
   )
 );
