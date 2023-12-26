252cb32ed4ba6206c4e7aa6d049a831d8f15a57a
barrytran209 <barry.tran@manadr.com>
2023-12-26 17:40:52 +0700

    Update Layout

--- src/Layout/ProtectedLayout.js
+++ src/Layout/ProtectedLayout.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import React, { useState } from 'react';
+import { Layout, Menu, Button, theme, PageHeader, Flex } from 'antd';
 import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
   UserOutlined,
   VideoCameraOutlined,
-  UploadOutlined,
 } from '@ant-design/icons';
 import { useNavigate, Navigate, useOutlet } from 'react-router-dom';
 import { useAuth } from '../Hook/useAuth';
@@ -11,35 +12,75 @@ import { useAuth } from '../Hook/useAuth';
 
 const { Header, Sider, Content } = Layout;
 
+
 export const ProtectedLayout = () => {
   const { accessToken } = useAuth();
   console.log('accessToken:', accessToken);
   const navigate = useNavigate();
   const outlet = useOutlet();
+  const [collapsed, setCollapsed] = useState(false);
+  const [currentPage, setCurrentPage] = useState('Dashboard'); 
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  const handleMenuClick = (item) => {
+    setCurrentPage(item.label);};
 
   if (!accessToken) {
     return <Navigate to="/login" />;
   }
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider width={80} theme="dark" collapsible>
-        {/* Sidebar content */}
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1" icon={<UserOutlined />} />
-          <Menu.Item key="2" icon={<VideoCameraOutlined />} />
-          <Menu.Item key="3" icon={<UploadOutlined />} />
-        </Menu>
+    <Layout style={{ height: Flex }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'Dashboard',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'Settings',
+            },
+          ]}
+        />
       </Sider>
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }} />
-        <Content style={{ margin: '0 16px' }}>
-          <div
-            className="site-layout-background"
-            style={{ padding: 24, minHeight: 360 }}
-          >
-            {outlet}
-          </div>
+
+      <Layout>
+        <Header style={{ display: 'flex', padding: 0, background: colorBgContainer, alignItems: 'center'}}>
+      
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: '16px',
+                width: 64,
+                height: 64,
+              }}
+            />
+            <h2>{currentPage}</h2>
+   
+
+        </Header>
+        <Content
+          style={{
+            margin: '24px 24px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          {outlet}
         </Content>
       </Layout>
     </Layout>
