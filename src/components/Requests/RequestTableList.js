6014ebda4dfc58d6bf9c43808aebb6607c8d7767
huyngv6868 <huyngv6868@gmail.com>
2023-12-28 17:54:43 +0700

    exclude weekend

--- src/components/Requests/RequestTableList.js
+++ src/components/Requests/RequestTableList.js
@@ -8,7 +8,7 @@ import {
     FormOutlined, DeleteOutlined
 } from '@ant-design/icons';
 import axios from 'axios';
-import { differenceInDays, format, getYear } from 'date-fns';
+import { differenceInDays, format, getYear, isWeekend, isSameDay } from 'date-fns';
 
 const { confirm } = Modal;
 
@@ -50,8 +50,25 @@ const RequestTableList = () => {
             title: 'Leave Duration',
             key: 'leaveDuration',
             render: (_, record) => {
-                const duration = differenceInDays(record.endDate, record.startDate);
-                return `${duration + 1} days`;
+                const startDate = new Date(record.startDate);
+                const endDate = new Date(record.endDate);
+    
+                if (isSameDay(startDate, endDate)) {
+                    return '1 day';
+                }
+    
+                let days = differenceInDays(endDate, startDate) + 1;
+    
+                // Exclude weekends
+                for (let i = 0; i <= days; i++) {
+                    const currentDate = new Date(startDate);
+                    currentDate.setDate(startDate.getDate() + i);
+                    if (isWeekend(currentDate)) {
+                        days -= 1;
+                    }
+                }
+    
+                return `${days} days`;
             },
         },
         {
