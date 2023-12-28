c8cb1203f0e97c29dc5f35c3134814c15fb3139a
sylvia-t5 <trangnt.37ftu@gmail.com>
2023-12-28 14:34:30 +0700

    update request listing

--- src/Component/Home/RequestListing.js
+++ src/Component/Home/RequestListing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from "react";
+import axios from "axios"
 import '../../App.css';
 import { Space, Table, Select, Button, Flex, Result } from 'antd';
 import { FormOutlined, MoreOutlined } from '@ant-design/icons';
@@ -7,118 +8,61 @@ import { FormOutlined, MoreOutlined } from '@ant-design/icons';
 
 
 
-const handleChange = (value) => {
-    console.log(`selected ${value}`);
+const handleChange = (value, field) => {
+    // Add logic for handling different Select components
+    console.log(`Selected ${value} for ${field}`);
+    // Implement specific logic for each Select component's value change
 };
-/* const [columns,setColumns] = useState( [
-    {
-        title: '#',
-        dataIndex: 'id',
-    
-    },
-    {
-        title: 'Requester',
-        dataIndex: 'requester',
-        //render: (text) => <a>{text}</a>,
-    },
-    {
-        title: 'Leave Duration',
-        dataIndex: 'duration',
-    },
-    {
-        title: 'Leave Dates',
-        dataIndex: 'dates',
-    },
-    {
-        title: 'Note',
-        dataIndex: 'note',
-    },
-    {
-        title: 'Sent on Slack at',
-        dataIndex: 'sentslacktime',
-    }, 
-
-    {
-        title: 'Actions',
-        key: 'action',
-        render: (_, record) => (
-            <Space>
-                <FormOutlined />
-                <MoreOutlined />
-
-            </Space>
-        ),
-    },*/
-/* const [dataSource, setDataSource] = useState ([]);
-
-useEffect(() => {
-    fetch("https://dummyjson.com/quotes")
-    .then((res) => res.json())
-    .then((result) => {
-        setDataSource(result.quotes);
-    });
-}, []);
-    {
-        no: '1',
-        requester: 'Sylvia Nguyen',
-        duration: '2 days',
-        dates: '21 Dec 2023, 22 Dec 2023',
-        note: 'sick leave',
-        sentslacktime: '14 Dec · 7:00 PM',
-    },
-    {
-        no: '2',
-        requester: 'Ann Nguyen',
-        duration: '2 days',
-        dates: '21 Dec 2023, 22 Dec 2023',
-        note: 'sick leave',
-        sentslacktime: '14 Dec · 7:00 PM',
-    },
-    {
-        no: '3',
-        requester: 'Leo Nguyen',
-        duration: '2 days',
-        dates: '21 Dec 2023, 22 Dec 2023',
-        note: 'sick leave',
-        sentslacktime: '14 Dec · 7:00 PM',
-    },
-    {
-        no: '4',
-        requester: 'Kevin Nguyen',
-        duration: '2 days',
-        dates: '21 Dec 2023, 22 Dec 2023',
-        note: 'sick leave',
-        sentslacktime: '14 Dec · 7:00 PM',
-    },
-    {
-        no: '5',
-        requester: 'Sylvia Nguyen',
-        duration: '2 days',
-        dates: '21 Dec 2023, 22 Dec 2023',
-        note: 'sick leave',
-        sentslacktime: '14 Dec · 7:00 PM',
-    },*/
-;
-
 const RequestListing = () => {
-    const [columns, setColumns] = useState([  ]);
     const [dataSource, setDataSource] = useState([]);
+    const columns = [
+        {
+            key: '1',
+            title: '#',
+            dataIndex: 'index',
+        
+        },
+        {
+            key: '2',
+            title: 'Requester',
+            dataIndex: '_id',
+        },
+        {
+           key: '3',
+            title: 'Leave Dates',
+            dataIndex: 'startDate',
+        },
+        {
+            key: '4',
+            title: 'Note',
+            dataIndex: 'note',
+        },
+        {
+            key: '5',
+            title: 'Sent on Slack at',
+            dataIndex: 'statusOnSlack',
+        }, 
+    
+        {
+            key: '6',
+            title: 'Actions',
+            render: (_, record) => (
+                <Space>
+                    <FormOutlined />
+                    <MoreOutlined />
+    
+                </Space>
+            ),
+        }];
+    
+    const dataType = "requests";
 
     useEffect(() => {
-        fetch("https://dummyjson.com/quotes")
-            .then((res) => res.json())
-            .then((result) => {
-                const list = result.quotes || [];
-                const firstObject = list[0] || {
-                const col = {
-                    title: key,
-                    dataIndex: key,
-                };
-                close.push(col);
-            }
-            setColumns(cols)
-    },
-
+        
+        const result = axios.get(`http://localhost:7003/api/${dataType}`)
+            .then(res => setDataSource(res.data))
+            .catch(err => console.log(err));
+    },[]);
     return (
         <div className="request-listing-container">
             <div className="request-listing-header">
@@ -126,92 +70,45 @@ const RequestListing = () => {
                     <label className="typo">Date</label>
                     <Select
                         defaultValue="Today"
-                        style={{
-                            width: 120,
-                        }}
-                        onChange={handleChange}
+                        style={{ width: 120 }}
+                        onChange={(value) => handleChange(value, 'Date')}
                         options={[
-                            {
-                                value: 'Today',
-                                label: 'Today',
-                            },
-                            {
-                                value: 'This week',
-                                label: 'This week',
-                            },
-                            {
-                                value: 'This month',
-                                label: 'This month',
-                            },
-                            {
-                                value: 'This year',
-                                label: 'This year',
-                                disabled: true,
-                            },
+                            // Options for Date Select
                         ]}
                     />
                     <label className="typo">Member</label>
                     <Select
                         defaultValue="All"
-                        style={{
-                            width: 120,
-                        }}
-                        onChange={handleChange}
+                        style={{ width: 120 }}
+                        onChange={(value) => handleChange(value, 'Member')}
                         options={[
-                            {
-                                value: 'All',
-                                label: 'All',
-                            },
-                            {
-                                value: 'Sylvia',
-                                label: 'Sylvia',
-                            },
-                            {
-                                value: 'Ann',
-                                label: 'Ann',
-                            },
-                            {
-                                value: 'Cara',
-                                label: 'Cara',
-                            },
-                            {
-                                value: 'Nellie',
-                                label: 'Nellie',
-                            },
-                            {
-                                value: 'Kevin',
-                                label: 'Kevin',
-                            },
-                            {
-                                value: 'Barry',
-                                label: 'Barry',
-                            },
-                            {
-                                value: 'Leo',
-                                label: 'Leo',
-                            },
-
+                            // Options for Member Select
                         ]}
                     />
                 </Space>
                 <Flex gap="small" wrap="wrap">
-                    <Button type="primary" className="primary-button">Create Leave Request</Button>
+                    <Button type="primary" className="primary-button">
+                        Create Leave Request
+                    </Button>
                 </Flex>
-                </div>
-            <div className="request-listing-table" >
-                <Table
-                        columns={columns}
-                        dataSource={dataSource}
-                        bordered
-                        scroll={{ y: 900 }}
-                        >
-  
-                        </Table>
-                    </div>
-        
-                </div>
-        
-        
-            );
-        }
-                export default RequestListing;fix
+            </div>
+            <div className="request-listing-table">
+                <Table 
+                columns={columns} 
+                dataSource={dataSource.map((requests, dataIndex) => (
+                    <tr key={dataIndex}>
+                        <td>{requests.index}</td>
+                        <td>{requests._id}</td>
+                        <td>{requests.startDate}</td>
+                        <td>{requests.note}</td>
+                    </tr>
+                ))} 
+                bordered 
+                scroll={{ y: 900 }} />
+            </div>
+        </div>
+    );
+                };
+
+
+export default RequestListing;
