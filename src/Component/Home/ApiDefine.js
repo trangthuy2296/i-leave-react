889f0b074fc2c53aeafd3096f597ca57ad6fdaaf
sylvia-t5 <trangnt.37ftu@gmail.com>
2024-01-03 09:35:19 +0700

    update api define

--- src/Component/Home/ApiDefine.js
+++ src/Component/Home/ApiDefine.js
@@ -10,8 +10,8 @@ const api = axios.create({
 
 api.interceptors.request.use(function (config) {
     const token = JSON.parse(localStorage.getItem('accessToken'));
-    console.log(token.accessToken);
-    config.headers.Authorization = token? `Bearer ${token.accessToken}` : '';
+    console.log(token);
+    config.headers.Authorization = token? `Bearer ${token}` : '';
     return config;
 });
 
