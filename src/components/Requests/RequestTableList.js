ecc374e590cbe4e4f153bcae41a137bff259308b
huyngv6868 <huyngv6868@gmail.com>
2024-01-03 15:32:36 +0700

    1

--- src/components/Requests/RequestTableList.js
+++ src/components/Requests/RequestTableList.js
@@ -63,164 +63,162 @@ const RequestTableList = ({ dateFilter, selectedMember, members }) => {
             console.log('fromDate:', fromDate);
             console.log('toDate:', toDate);
 
-            const params = {
-                fromDate,
-                toDate,
-                limit: 1000,
-                offset: 0,
-            };
-    
-            if (selectedMember !== 'all') {
-                params.userId = selectedMember;
-            }
-    
             const response = await axios.get('http://localhost:7003/api/requests', {
-                params: params,
+                params: {
+                    fromDate,
+                    toDate,
+                    userId: 'all',
+                    limit: 1000,
+                    offset: 0,
+                },
+                headers: {
+                    Authorization: accessToken ? `Bearer ${accessToken}` : undefined,
+                },
+            
             });
-    
 
-            console.log('selectedMember', selectedMember );
-            console.log('API Response:', response);
+        console.log('selectedMember', selectedMember);
+        console.log('API Response:', response);
 
-            if (response.data && Array.isArray(response.data.requests)) {
-                const dataWithIndex = response.data.requests.map((item, index) => ({
-                    ...item,
-                    key: (index + 1).toString(),
-                }));
-                setDataSource(dataWithIndex);
-            } else {
-                console.error('API response data.requests property is not an array:', response.data);
-            }
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            setDataSource([]);
+        if (response.data && Array.isArray(response.data.requests)) {
+            const dataWithIndex = response.data.requests.map((item, index) => ({
+                ...item,
+                key: (index + 1).toString(),
+            }));
+            setDataSource(dataWithIndex);
+        } else {
+            console.error('API response data.requests property is not an array:', response.data);
         }
-    };
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        setDataSource([]);
+    }
+};
 
 
-    useEffect(() => {
-        (async () => {
-            await fetchData();
-        })();
-    }, [dateFilter, selectedMember, members]);
+useEffect(() => {
+    (async () => {
+        await fetchData();
+    })();
+}, [dateFilter, selectedMember]);
 
 
-    const columns = [
-        {
-            title: '#',
-            dataIndex: 'key',
-            sort: (a, b) => a.key.localeCompare(b.key),
-        },
-        {
-            title: 'Requester',
-            key: 'createdBy.name',
-            render: (text, record) => record.createdBy.name,
-            sorter: (a, b) => a.createdBy.name.localeCompare(b.createdBy.name),
-        },
-        {
-            title: 'Leave Duration',
-            key: 'leaveDuration',
-            render: (_, record) => {
-                const startDate = new Date(record.startDate);
-                const endDate = new Date(record.endDate);
+const columns = [
+    {
+        title: '#',
+        dataIndex: 'key',
+        sort: (a, b) => a.key.localeCompare(b.key),
+    },
+    {
+        title: 'Requester',
+        key: 'createdBy.name',
+        render: (text, record) => record.createdBy.name,
+        sorter: (a, b) => a.createdBy.name.localeCompare(b.createdBy.name),
+    },
+    {
+        title: 'Leave Duration',
+        key: 'leaveDuration',
+        render: (_, record) => {
+            const startDate = new Date(record.startDate);
+            const endDate = new Date(record.endDate);
 
-                if (isSameDay(startDate, endDate)) {
-                    return '1 day';
+            if (isSameDay(startDate, endDate)) {
+                return '1 day';
+            }
+
+            let days = differenceInDays(endDate, startDate) + 1;
+
+            // Exclude weekends
+            for (let i = 0; i <= days; i++) {
+                const currentDate = new Date(startDate);
+                currentDate.setDate(startDate.getDate() + i);
+                if (isWeekend(currentDate)) {
+                    days -= 1;
                 }
+            }
 
-                let days = differenceInDays(endDate, startDate) + 1;
+            return `${days} days`;
+        },
+        sorter: (a, b) => {
+            // Leave duration comparison logic
+        },
+    },
+    {
+        title: 'Leave Dates',
+        dataIndex: 'leaveDates',
+        key: 'leaveDates',
+        render: (_, record) => {
+            const startDateFormatted = format(record.startDate, 'dd MMM');
+            const endDateFormatted = format(record.endDate, 'dd MMM');
 
-                // Exclude weekends
-                for (let i = 0; i <= days; i++) {
-                    const currentDate = new Date(startDate);
-                    currentDate.setDate(startDate.getDate() + i);
-                    if (isWeekend(currentDate)) {
-                        days -= 1;
-                    }
-                }
-
-                return `${days} days`;
-            },
-            sorter: (a, b) => {
-                // Leave duration comparison logic
-            },
+            return startDateFormatted === endDateFormatted
+                ? startDateFormatted
+                : `${startDateFormatted} - ${endDateFormatted} ${getYear(record.startDate)}`;
         },
-        {
-            title: 'Leave Dates',
-            dataIndex: 'leaveDates',
-            key: 'leaveDates',
-            render: (_, record) => {
-                const startDateFormatted = format(record.startDate, 'dd MMM');
-                const endDateFormatted = format(record.endDate, 'dd MMM');
-
-                return startDateFormatted === endDateFormatted
-                    ? startDateFormatted
-                    : `${startDateFormatted} - ${endDateFormatted} ${getYear(record.startDate)}` ;
-            },
-            sorter: (a, b) => new Date(a.startDate) - new Date(b.startDate),
-        },
-        {
-            title: 'Notes',
-            dataIndex: 'note',
-            key: 'note',
-        },
-        {
-            title: 'Sent on Slack at',
-            dataIndex: 'statusOnSlack',
-            key: 'statusOnSlack',
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            align: 'center',
-            render: (text, record) => (
-                <Space size='small'>
+        sorter: (a, b) => new Date(a.startDate) - new Date(b.startDate),
+    },
+    {
+        title: 'Notes',
+        dataIndex: 'note',
+        key: 'note',
+    },
+    {
+        title: 'Sent on Slack at',
+        dataIndex: 'statusOnSlack',
+        key: 'statusOnSlack',
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        align: 'center',
+        render: (text, record) => (
+            <Space size='small'>
+                <Button
+                    type='link'
+                    icon={<FormOutlined />}
+                    onClick={() => handleEdit(record.key)}
+                >
+                </Button>
+                <Popconfirm
+                    title="Are you sure you want to delete this request?"
+                    onConfirm={() => handleDelete(record._id)}
+                    okText="Delete"
+                    cancelText="Cancel"
+                >
                     <Button
-                        type='link'
-                        icon={<FormOutlined />}
-                        onClick={() => handleEdit(record.key)}
+                        type='danger'
+                        icon={<DeleteOutlined />}
+                        style={{ color: 'red' }}
                     >
                     </Button>
-                    <Popconfirm
-                        title="Are you sure you want to delete this request?"
-                        onConfirm={() => handleDelete(record._id)}
-                        okText="Delete"
-                        cancelText="Cancel"
-                    >
-                        <Button
-                            type='danger'
-                            icon={<DeleteOutlined />}
-                            style={{ color: 'red' }}
-                        >
-                        </Button>
-                    </Popconfirm>
-                </Space>
-            ),
-        },
-    ];
+                </Popconfirm>
+            </Space>
+        ),
+    },
+];
 
-    const handleEdit = (key) => {
-        // Add edit logic here
-        console.log(`Edit request with key: ${key}`);
-    };
+const handleEdit = (key) => {
+    // Add edit logic here
+    console.log(`Edit request with key: ${key}`);
+};
 
-    const handleDelete = async (_id) => {
-        try {
-            const response = await axios.delete(`http://localhost:7003/api/requests/${_id}`);
+const handleDelete = async (_id) => {
+    try {
+        const response = await axios.delete(`http://localhost:7003/api/requests/${_id}`);
 
-            if (response.status === 200) {
-                // Request successfully deleted, you may want to update the local state or refetch data
-                console.log(`Request with key ${_id} deleted successfully`);
-                window.location.reload();
-            } else {
-                console.error(`Failed to delete request with key ${_id}`);
-            }
-        } catch (error) {
-            console.error('Error deleting request:', error);
+        if (response.status === 200) {
+            // Request successfully deleted, you may want to update the local state or refetch data
+            console.log(`Request with key ${_id} deleted successfully`);
+            window.location.reload();
+        } else {
+            console.error(`Failed to delete request with key ${_id}`);
         }
-    };
+    } catch (error) {
+        console.error('Error deleting request:', error);
+    }
+};
 
-    return <Table dataSource={dataSource} columns={columns} />;
+return <Table dataSource={dataSource} columns={columns} />;
 };
 
 export default RequestTableList;
