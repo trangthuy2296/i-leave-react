59492ac49f7bbd2a87984a0f0258281f5c96a856
trangnaii <trangthuy2296@gmail.com>
2024-01-15 11:10:24 +0700

    Add ecosystem file

---
+++ ecosystem.prod.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    apps: [
+      {
+        name: 'ileave-react-prod',
+        script: 'pm2 serve ./build 6666 --name "ileave-react" --spa',
+        'env_production': {
+          'NODE_ENV': 'production',
+          'CONFIG_FILE': 'prod.env',
+        },
+      },
+    ],
+
+    deploy: {
+      production: {
+        user: 'root',
+        host: 'ileave.prod',
+        ref: 'origin/main',
+        repo: 'git@i-leave-react.github.com:trangthuy2296/i-leave-react.git',
+        path: '/root/apps/i-leave-react/source',
+        'pre-deploy-local': '',
+        'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.prod.config.js --env production',
+        'pre-setup': '',
+      },
+    },
+  };
