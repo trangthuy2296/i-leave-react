25d6932ee1511735d01499afb05930ae74b369d4
kevin <>
2024-01-15 15:06:50 +0700

    clean up

--- bin/proxy.js
+++ bin/proxy.js
@@ -3,11 +3,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const router = Express.Router();
 
-const LOG_LEVEL = process.env.PROXY_LOG_LEVEL || 'info'; // ['debug', 'info', 'warn', 'error', 'silent']
+const LOG_LEVEL = process.env.PROXY_LOG_LEVEL || 'debug'; // ['debug', 'info', 'warn', 'error', 'silent']
 
 router.use('/api', createProxyMiddleware({
   changeOrigin: true,
-  pathRewrite: { '^/api': '' },
+  //pathRewrite: { '^/api': '' },
   target: 'http://localhost:7003',
   logLevel: LOG_LEVEL,
 }));
